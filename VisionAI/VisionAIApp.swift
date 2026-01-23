7e2822d379726be42e65fc024799d50c87f9c1fb
somiljain2006 <somil16022006@gmail.com>
2026-01-24 01:28:40 +0530

    Initial Commit

---
+++ VisionAI/VisionAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionAIApp.swift
+//  VisionAI
+//
+//  Created by somil jain on 24/01/26.
+//
+
+import SwiftUI
+
+@main
+struct VisionAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
